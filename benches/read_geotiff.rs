c22b24376bc2d1e52bad011a2ba65c1361420fb4
Wei Ji <23487320+weiji14@users.noreply.github.com>
2025-11-10 14:54:28 +1300

    :green_heart: Add criterion_benchmark func to benches/read_geotiff.rs
    Can't be too empty. Also renamed ci.yml to bench.yml and explicitly set mode: instrumentation on CodSpeedHQ/action.

--- benches/read_geotiff.rs
+++ benches/read_geotiff.rs
@@ -1,1 +1,10 @@
+use criterion::{Criterion, criterion_group, criterion_main};
 
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("read_geotiff");
+
+    group.finish();
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
