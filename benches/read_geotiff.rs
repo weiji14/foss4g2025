14d9b2706bc5d87dbcea5520bbdbfcd8beb2fe57
Wei Ji <23487320+weiji14@users.noreply.github.com>
2025-11-12 17:50:18 +1300

    :alembic: Benchmark nvTIFF CUDA GPU-based decoding (#4)
    * :heavy_plus_sign: Add cog3pio with 'cuda' feature flag
    
    Specifically a version with the experimental `CudaCogReader` struct that can read a GeoTIFF into CUDA memory via DLPack. Also installed bytes, and upgraded dlpark to a newer version with CudaView impl feature.
    
    * :alembic: Benchmark nvTIFF CUDA GPU-based decoding
    
    Run benchmarks reading the LZW-compressed GeoTIFF to CUDA GPU memory via DLPack. Using cog3pio's CudaCogReader which uses bindings to the nvTIFF library.
    
    * :truck: Perform host to device copy for CPU benchmarks
    
    When 'cuda' feature flag is enabled, copy decoded bytes from host (CPU) to device (GPU) to allow fair comparison with nvTIFF benchmark where data resides in CUDA memory. Well, not exactly fair since nvTIFF is winning, but still need this.
    
    Note that async-tiff's decoded byte length seems longer than expected, not sure why... Added some extra docs and links to the main README.md too.
    
    * :recycle: Collapse async-tiff tile decode into single flat_map_iter call
    
    No need for separate `.flat_map` and `.map`. Can coerce Bytes into u8 directly apparently. Still need to figure out if there's a more efficient way of multi-threaded decoding to raw bytes though.

--- benches/read_geotiff.rs
+++ benches/read_geotiff.rs
@@ -1,6 +1,7 @@
-// Benchmark tests on reading a GeoTIFF into memory
+// Benchmark tests on reading a GeoTIFF into memory (CPU or GPU)
 //
 // Libraries compared:
+// - nvTIFF (Enable NVIDIA network repository and do `sudo apt install nvtiff nvcomp-cuda-12`)
 // - GDAL
 // - async-tiff
 //
@@ -11,19 +12,31 @@
 // - Change from DEFLATE to LZW compression with Horizontal differencing predictor (272.2MB)
 //   using the following command
 //   `gdal raster convert --co COMPRESS=LZW --co TILED=YES --co PREDICTOR=2 benches/TCI.tif benches/TCI_lzw.tif`
-// - Run `cargo bench`
+// - Run `cargo bench` (CPU-only) or `cargo bench --features cuda` (with CUDA-enabled GPU)
 //
 // References:
 // - https://github.com/microsoft/pytorch-cloud-geotiff-optimization/blob/5fb6d1294163beff822441829dcd63a3791b7808/configs/search.yaml#L6
 
 use std::path::PathBuf;
 use std::sync::Arc;
+#[cfg(feature = "cuda")]
+use std::time::Duration;
 
 use async_tiff::decoder::DecoderRegistry;
 use async_tiff::metadata::{PrefetchBuffer, TiffMetadataReader};
 use async_tiff::reader::ObjectReader;
 use async_tiff::{ImageFileDirectory, Tile};
+#[cfg(feature = "cuda")]
+use bytes::Bytes;
+#[cfg(feature = "cuda")]
+use cog3pio::io::nvtiff::CudaCogReader;
 use criterion::{BenchmarkId, Criterion, Throughput, criterion_group, criterion_main};
+#[cfg(feature = "cuda")]
+use cudarc::driver::{CudaContext, CudaStream};
+#[cfg(feature = "cuda")]
+use dlpark::SafeManagedTensorVersioned;
+#[cfg(feature = "cuda")]
+use dlpark::traits::TensorView;
 use gdal::raster::Buffer;
 use gdal::{Dataset, DatasetOptions, GdalOpenFlags};
 use ndarray::Array2;
@@ -33,6 +46,21 @@ use rayon::ThreadPoolBuilder;
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use tokio::runtime;
 use url::Url;
+
+// nvtiff
+#[cfg(feature = "cuda")]
+fn read_geotiff_nvtiff(fpath: &str) {
+    let v: Vec<u8> = std::fs::read(fpath).unwrap();
+    let bytes = Bytes::copy_from_slice(&v);
+
+    let ctx: Arc<CudaContext> = CudaContext::new(0).unwrap(); // Set on GPU:0
+    let cuda_stream: Arc<CudaStream> = ctx.default_stream();
+
+    let cog = CudaCogReader::new(&bytes, &cuda_stream).unwrap();
+    let tensor: SafeManagedTensorVersioned = cog.dlpack().unwrap();
+
+    assert_eq!(tensor.num_elements(), 3 * 10980 * 10980);
+}
 
 // gdal
 fn read_geotiff_gdal(fpath: &str, n_threads: usize) {
@@ -48,9 +76,21 @@ fn read_geotiff_gdal(fpath: &str, n_threads: usize) {
     for b in 1..3 {
         let band = ds.rasterband(b).unwrap();
         let buffer: Buffer<u8> = band.read_band_as::<u8>().unwrap();
-        let array: Array2<_> = buffer.to_array().unwrap();
+        let mut array: Array2<_> = buffer.to_array().unwrap();
 
         assert_eq!(array.shape(), [10980, 10980]);
+
+        #[cfg(feature = "cuda")]
+        {
+            // Copy from CPU (host) memory to CUDA (device) memory
+            let ctx: Arc<CudaContext> = CudaContext::new(0).unwrap(); // Set on GPU:0
+            let cuda_stream: Arc<CudaStream> = ctx.default_stream();
+            let mut cuda_mem = cuda_stream.alloc_zeros::<u8>(3 * 10980 * 10980).unwrap();
+
+            cuda_stream
+                .memcpy_htod(array.as_slice_mut().unwrap(), &mut cuda_mem)
+                .unwrap();
+        }
     }
 }
 
@@ -93,10 +133,10 @@ fn read_geotiff_async_tiff(fpath: &str, n_threads: usize) {
         let (x_count, y_count) = ifd.tile_count().unwrap();
         // dbg!(x_count, y_count); // 43 * 43 = 1849
         // Get cartesian product of x and y tile ids
-        let x_ids: Vec<_> = (0..x_count)
+        let x_ids: Vec<usize> = (0..x_count)
             .flat_map(|i| (0..y_count).map(move |_j| i))
             .collect();
-        let y_ids: Vec<_> = (0..x_count).flat_map(|_i| 0..y_count).collect();
+        let y_ids: Vec<usize> = (0..x_count).flat_map(|_i| 0..y_count).collect();
 
         let tiles: Vec<Tile> = ifd.fetch_tiles(&x_ids, &y_ids, &reader).await.unwrap();
         assert_eq!(tiles.len(), 1849);
@@ -109,13 +149,27 @@ fn read_geotiff_async_tiff(fpath: &str, n_threads: usize) {
         .num_threads(n_threads)
         .build()
         .unwrap();
-    let tile_bytes: Vec<_> = pool.install(|| {
+    let tile_bytes: Vec<u8> = pool.install(|| {
         tiles
             .into_par_iter()
-            .map(|tile| tile.decode(&decoder_registry).unwrap())
+            .flat_map_iter(|tile| tile.decode(&decoder_registry).unwrap())
             .collect()
     });
-    assert_eq!(tile_bytes.len(), 1849);
+    assert_eq!(tile_bytes.len(), 363528192); // should be 361681200, why not?
+
+    #[cfg(feature = "cuda")]
+    {
+        // Copy from CPU (host) memory to CUDA (device) memory
+        let ctx: Arc<CudaContext> = CudaContext::new(0).unwrap(); // Set on GPU:0
+        let cuda_stream: Arc<CudaStream> = ctx.default_stream();
+        let mut cuda_mem = cuda_stream
+            .alloc_zeros::<u8>(
+                tile_bytes.len(), // should be 3 * 10980 * 10980 theoretically
+            )
+            .unwrap();
+
+        cuda_stream.memcpy_htod(&tile_bytes, &mut cuda_mem).unwrap();
+    }
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
@@ -123,6 +177,21 @@ fn criterion_benchmark(c: &mut Criterion) {
 
     let fsize: u64 = std::fs::metadata("benches/TCI_lzw.tif").unwrap().len();
     group.throughput(Throughput::BytesDecimal(fsize)); // 272.2MB filesize
+
+    // GPU decoding using nvTIFF
+    #[cfg(feature = "cuda")]
+    {
+        group
+            .sample_size(10) // reduce sample size because of CUDA memory leak
+            .nresamples(2)
+            .warm_up_time(Duration::from_millis(1))
+            .measurement_time(Duration::from_secs(2));
+        group.bench_with_input(
+            BenchmarkId::new("0_nvTIFF_GPU", "Sentinel-2 TCI"),
+            "benches/TCI_lzw.tif",
+            |b, p| b.iter(|| read_geotiff_nvtiff(p)),
+        );
+    }
 
     // CPU decoding using GDAL
     group.sample_size(30);
